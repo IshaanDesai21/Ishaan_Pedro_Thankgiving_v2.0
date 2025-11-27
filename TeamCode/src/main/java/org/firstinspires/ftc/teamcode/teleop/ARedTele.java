1e999521e7200156f7e91e3fc7c6f34ce91f60b9
ishaandesai <IshaanDesai21@gmail.com>
2025-11-26 23:47:21 -0600

    Migrate Tele

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ARedTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ARedTele.java
@@ -1,4 +1,5 @@
 package org.firstinspires.ftc.teamcode.teleop;
+
 import com.bylazar.configurables.annotations.Configurable;
 import com.bylazar.telemetry.PanelsTelemetry;
 import com.bylazar.telemetry.TelemetryManager;
@@ -15,32 +16,24 @@ import org.firstinspires.ftc.teamcode.misc.gamepad.GamepadMapping;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 import org.firstinspires.ftc.teamcode.subsystems.robot.Robot;
 import org.firstinspires.ftc.teamcode.teleop.fsm.FSM;
+import org.firstinspires.ftc.teamcode.teleop.fsm.IshaanFSM;
 
 import java.util.function.Supplier;
 
 @Configurable
 @TeleOp
-public class ExampleTeleOp extends OpMode {
-
+public class ARedTele extends OpMode {
 
     private GamepadMapping controls;
     private FSM fsm;
     private Robot robot;
-
-
     private Follower follower;
     private boolean automatedDrive;
     private Supplier<PathChain> pathChain;
     private TelemetryManager telemetryM;
-    private boolean slowMode = false;
-    private double slowModeMultiplier = 0.5;
-
 
     private boolean autoTurn = false;
     private double goalHeading = 0;
-    private final double GOAL_X = 142;
-    private final double GOAL_Y = 142;
-
 
     @Override
     public void init() {
@@ -48,51 +41,35 @@ public class ExampleTeleOp extends OpMode {
         robot = new Robot(hardwareMap, controls);
         fsm = new FSM(hardwareMap, controls, robot);
 
-
-
         follower = Constants.createFollower(hardwareMap);
         follower.setStartingPose(new Pose(126, 118, Math.toRadians(36)));
         follower.update();
         telemetryM = PanelsTelemetry.INSTANCE.getTelemetry();
 
-
-        pathChain = () -> follower.pathBuilder()
-                .addPath(
-                        new Path(
-                                new BezierLine(
-                                        follower::getPose,
-                                        new Pose(follower.getPose().getX() + 0.1,
-                                                follower.getPose().getY() + 0.1)
-                                )
-                        )
-                )
-                .setHeadingInterpolation(
-                        HeadingInterpolator.linearFromPoint(
-                                follower::getHeading,
-                                () -> {   // <--- Dynamic heading supplier
-                                    double robotX = follower.getPose().getX();
+        pathChain = () -> follower.pathBuilder() .addPath(
+                        new Path(new BezierLine(
+                                follower::getPose, new Pose(follower.getPose().getX() + 0.01, follower.getPose().getY() + 0.01))
+                        )) .setHeadingInterpolation(
+                        HeadingInterpolator.linearFromPoint(follower::getHeading,
+                                () -> { double robotX = follower.getPose().getX();
                                     double robotY = follower.getPose().getY();
-                                    return Math.atan2(142 - robotY, 142 - robotX);
-                                },
-                                0.98
-                        )
-                )
+                                    return Math.atan2(142 - robotY, 142 - robotX); },
+                                0.98))
                 .build();
-
     }
 
     @Override
     public void start() {
         robot.hardwareSoftReset();
         follower.startTeleopDrive(true);
-
     }
-
-
 
     @Override
     public void loop() {
+        // Update FSM
         fsm.update();
+
+        // Update follower & telemetry
         follower.update();
         telemetryM.update();
 
@@ -101,52 +78,52 @@ public class ExampleTeleOp extends OpMode {
         double y = p.getY();
         double heading = p.getHeading();
 
+        // Reset pose
+        if (gamepad1.xWasPressed()) {
+            follower.setPose(new Pose(126, 118, Math.toRadians(36)));
+        }
+
+        // Heading lock / auto-turn
         if (gamepad1.a && !autoTurn) {
             autoTurn = true;
-            goalHeading = Math.atan2(GOAL_Y - y, GOAL_X - x);
+            goalHeading = Math.atan2(142 - y, 142 - x);
         }
 
         double headingError = angleWrap(goalHeading - heading);
         boolean turnFinished = Math.abs(headingError) < Math.toRadians(3);
+        if (autoTurn && turnFinished) autoTurn = false;
 
-        if (autoTurn && turnFinished) {
-            autoTurn = false;
-        }
-
+        // Driving inputs
         double forward = -gamepad1.left_stick_y;
         double strafe  = -gamepad1.left_stick_x;
         double rotate;
 
-        if (slowMode) {
-            forward *= slowModeMultiplier;
-            strafe  *= slowModeMultiplier;
-        }
-
         if (autoTurn) {
-            rotate = headingError * 1.8;  // Pedro heading PID gain
+            // Zero forward/strafe during auto-turn
+            forward = 0;
+            strafe = 0;
+            double Kp = 1.0;
+            rotate = headingError * Kp;
         } else {
             rotate = -gamepad1.right_stick_x;
-            if (slowMode) rotate *= slowModeMultiplier;
         }
 
         follower.setTeleOpDrive(forward, strafe, rotate, true);
 
+        // Automated path
         if (gamepad1.aWasPressed()) {
             follower.followPath(pathChain.get());
             automatedDrive = true;
         }
-
         if (automatedDrive && (gamepad1.bWasPressed() || !follower.isBusy())) {
             follower.startTeleopDrive();
             automatedDrive = false;
         }
 
-        if (gamepad1.rightBumperWasPressed()) slowMode = !slowMode;
-        if (gamepad1.xWasPressed()) slowModeMultiplier += 0.25;
-        if (gamepad2.yWasPressed()) slowModeMultiplier -= 0.25;
-
+        // Telemetry
         telemetryM.debug("pose", p);
         telemetryM.debug("autoTurn", autoTurn);
+        telemetry.addData("Heading", heading);
     }
 
     private double angleWrap(double a) {
@@ -154,7 +131,4 @@ public class ExampleTeleOp extends OpMode {
         while (a < -Math.PI) a += 2*Math.PI;
         return a;
     }
-
-
-
 }
